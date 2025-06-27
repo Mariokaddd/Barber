e3ecea53436135edc08810bd06c506983e060f30
Yoanna Kadiyska <webmaster1@silabg.com>
2025-06-27 22:36:28 +0300

    AddedTheFavoritesTable AndFunctunality

--- Barber/Program.cs
+++ Barber/Program.cs
@@ -17,9 +17,10 @@ namespace Barber
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+                options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
+
 
             var app = builder.Build();
 
